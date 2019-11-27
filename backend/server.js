1ba9635784ffd35c411d43627b966c6054e3d526
Saloni Kaur <missa4892@gmail.com>
2019-11-27 16:45:20 +0800

    drinksMenu REST from json

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const fs = require('fs');
+const drinksMenu = "./drinksMenu.json";
 
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+app.get('/drinksMenu', (req, res) => {
+    res.send(JSON.parse(fs.readFileSync(drinksMenu)));
 });
+
+app.post('/placeOrder', )
+
+// const dbConnection = mysql.createConnection({
+//     host: "remotemysql.com:3306",
+//     user: "tlvRWeCJZP",
+//     password: "P8aVD4Fm6d"
+//   });
+  
+//   dbConnection.connect((err) => {
+//     if (err) throw err;
+//     console.log("DB Connected!");
+//   });
 
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
 
